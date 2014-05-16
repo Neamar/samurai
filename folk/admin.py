f61395e775a8846606e5386c8cb5b61a78bb37da
Neamar <neamar@neamar.fr>
2014-05-16 17:11:22 +0200

    Admin display

--- folk/admin.py
+++ folk/admin.py
@@ -5,16 +5,35 @@ from folk.models import Chateau, Clan, Personne
 
 
 class ChateauAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('nom', 'thumb')
+
+	def thumb(self, obj):
+		return '<img src="%s" style="width:50px" />' % obj.avatar.url
+	thumb.short_description = 'Thumb'
+	thumb.allow_tags = True
 admin.site.register(Chateau, ChateauAdmin)
 
 
 class ClanAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('nom', 'thumb')
+
+	def thumb(self, obj):
+		return '<img src="%s" style="width:50px" />' % obj.avatar.url
+	thumb.short_description = 'Thumb'
+	thumb.allow_tags = True
+
 admin.site.register(Clan, ClanAdmin)
 
 
 class PersonneAdmin(admin.ModelAdmin):
-	list_display = ('nom', 'sexe')
-	list_filter = ('clan', 'plot', 'suzerain',)
+	list_display = ('thumb', 'nom', 'sexe', 'clan')
+	list_filter = ('clan', 'plot')
+
+	def thumb(self, obj):
+		return '<img src="%s" style="width:50px" />' % obj.avatar.url
+	thumb.short_description = 'Thumb'
+	thumb.allow_tags = True
+
+	def queryset(self, request):
+		return super(PersonneAdmin, self).queryset(request).select_related('clan')
 admin.site.register(Personne, PersonneAdmin)

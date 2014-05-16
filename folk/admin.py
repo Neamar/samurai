c13813b8e218b4114b4d8a21577c396c62a4ef60
Neamar <neamar@neamar.fr>
2014-05-16 16:55:36 +0200

    Model definition

--- folk/admin.py
+++ folk/admin.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from folk.models import Chateau, Clan, Personne
+
+
+class ChateauAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Chateau, ChateauAdmin)
+
+
+class ClanAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Clan, ClanAdmin)
+
+
+class PersonneAdmin(admin.ModelAdmin):
+	list_display = ('nom', 'sexe')
+	list_filter = ('clan', 'plot', 'suzerain',)
+admin.site.register(Personne, PersonneAdmin)
--- folk/models.py
+++ folk/models.py
@@ -1,17 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 class Chateau(models.Model):
+	class Meta:
+		verbose_name_plural = "Chateaux"
 	nom = models.CharField(max_length=100)
+	avatar = models.ImageField(upload_to="chateau")
 
 
 class Clan(models.Model):
 	nom = models.CharField(max_length=100)
+	avatar = models.ImageField(upload_to="clan")
+
+	def __unicode__(self):
+		return self.nom
 
 
 class Plot(models.Model):
 	nom = models.CharField(max_length=100)
 	description = models.TextField()
+
+	def __unicode__(self):
+		return self.nom
 
 
 class Personne(models.Model):
@@ -26,23 +37,26 @@ class Personne(models.Model):
 
 	nom = models.CharField(max_length=100)
 	sexe = models.CharField(max_length=1, choices=SEX_CHOICES, default=HOMME)
-
+	avatar = models.ImageField(upload_to="avatar")
 	clan = models.ForeignKey(Clan)
 
-	intrigue = models.SmallPositiveIntegerField()
-	combat = models.SmallPositiveIntegerField()
-	gestion = models.SmallPositiveIntegerField()
-	diplomatie = models.SmallPositiveIntegerField()
-	strategie = models.CharField(max_length=15)
+	intrigue = models.PositiveSmallIntegerField(default=1)
+	combat = models.PositiveSmallIntegerField(default=1)
+	gestion = models.PositiveSmallIntegerField(default=1)
+	diplomatie = models.PositiveSmallIntegerField(default=1)
+	strategie = models.CharField(max_length=15, blank=True, null=True)
 
 	pere = models.ForeignKey('self', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
 	mere = models.ForeignKey('self', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
 	femme = models.ForeignKey('self', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
-	fratrie = models.SmallPositiveIntegerField()
+	fratrie = models.PositiveSmallIntegerField(default=1)
 
-	chateau = models.ForeignKey(Chateau)
+	chateau = models.ForeignKey(Chateau, blank=True, null=True)
 	suzerain = models.ForeignKey('self', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
 
-	description = models.TextField()
-	notes = models.TextField()
-	plot = models.ForeignKey(Plot)
+	description = models.TextField(blank=True, null=True)
+	notes = models.TextField(blank=True, null=True)
+	plot = models.ForeignKey(Plot, blank=True, null=True)
+
+	def __unicode__(self):
+		return self.nom

0915585941586e43f1a317f4b37100e228bd69a6
Neamar <neamar@neamar.fr>
2015-08-17 15:17:33 +0200

    Improved admin

--- game/admin.py
+++ game/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from game.models import Perso, Clan, Lieu, Culture, Religion
 
-admin.site.register(Perso)
+
+@admin.register(Perso)
+class PersoAdmin(admin.ModelAdmin):
+    # fieldsets = (
+    #     (None, {
+    #         "fields": [f for f in Perso._meta.get_all_field_names() if f != "notes_mj"]
+    #     }),
+    #     ('Notes MJ', {
+    #         'classes': ('collapse',),
+    #         'fields': ('notes_mj',)
+    #     }),
+    # )
+    list_filter = ('sexe', 'clan', 'factions', 'culture', 'residence')
+    list_max_show_all = 5000
+    save_on_top = True
+    search_fields = ('name', 'clan__name')
+
 admin.site.register(Clan)
 admin.site.register(Lieu)
 admin.site.register(Culture)
